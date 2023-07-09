93a348de58f937746f39f4b770ca6e4ea7b5b9f0
ruby <jhonnatanwfc@live.com>
2023-07-09 10:25:59 -0300

    ball

--- breakout/game.js
+++ breakout/game.js
@@ -1,59 +1,57 @@
 let config = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     width: 1200,
     height: 700,
     backgroundColor: "#123524",
     fps: 60,
     scene: {
         create: create,
-        update: update
-    }
+        update: update,
+    },
 };
 
 let game = new Phaser.Game(config);
 let paddle;
 let keys;
-const paddle_vel = 300;
-let mouse_guide = false;
+const paddle_vel = 500;
 let player;
-let player_rect;
-let rect_w = 200;
-let rect_h = 30;
-let rect_container;
-//let grid;
+let player_w = 200;
+let player_h = 30;
 const box_width = 80;
 const box_height = 40;
 const gap = 22;
 let grid = [];
+let ball;
+let ball_speed = 300;
+let ball_vel = {x: 0, y: 0};
+let game_on = false;
 
 function create(){
-    let x = config.width / 2 - rect_w / 2;
-    let y = config.height - rect_h - 5;
-    keys = this.input.keyboard.addKeys("A,D,RIGHT,LEFT"); 
-    rect_container = this.add.container(x, y);
-    player = this.add.graphics();
-    player_rect = new Phaser.Geom.Rectangle(x, y, rect_w, rect_h);
-    player.fillStyle(0x6ed3a2);
-    player.fillRoundedRect(0, 0, rect_w, rect_h, 8);
-    rect_container.add(player);
-    //grid = this.add.container(0, 0);
-    for(let row = 0; row < 5; ++row){
+    let player_x = config.width / 2 - player_w / 2;
+    let player_y = config.height - player_h - 5;
+    keys = this.input.keyboard.addKeys("A,D,RIGHT,LEFT,SPACE"); 
+    // new Phaser.Geom.Rectangle(x, y, player_w, player_h);
+    player = this.add.rectangle(player_x, player_y, player_w, player_h, 0x6ed3a2);
+    player.setOrigin(0, 0);
+    //ball = new Phaser.Geom.Circle(x + player_w / 2, y - player_h / 2, 20);
+    ball = this.add.circle(player_x + player_w / 2, player_y - player_h / 2, 20, 0xff0000);
+    for(let row = 0; row < 4; ++row){
         grid[row] = [];
         for(let col = 0; col < config.width / box_width; ++col){
             let x = col * box_width + col * gap;
             let y = row * box_height + row * gap;
-            //grid[row][col] = this.add.rectangle(x + box_width / 2, y + box_height / 2, box_width, box_height, 0x89CFF0);
-            let box = this.add.graphics();
-            box.fillStyle(0x89CFF0);
-            box.fillRoundedRect(x, y, box_width, box_height, 15);
-            grid[row][col] = new Phaser.Geom.Rectangle(x, y, box_width, box_height);
+            // new Phaser.Geom.Rectangle(x, y, box_width, box_height);
+            grid[row][col] = this.add.rectangle(x, y, box_width, box_height, 0x89CFF0);
+            grid[row][col].setOrigin(0, 0);
         }
     }
+
 }
-  
+
+
 function check_boundaries(obj){
-    if(obj.x + rect_w > config.width){
-        obj.x = config.width - rect_w;
+    if(obj.x + player_w > config.width){
+        obj.x = config.width - player_w;
     }
     if(obj.x < 0){
         obj.x = 0;
@@ -68,11 +66,41 @@ function moveByArrow(obj, speed){
     }
 }
 
+function moveBall(ball_speed){
+    let ball_rect = new Phaser.Geom.Rectangle(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2);
+    if(Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), ball_rect)){
+        if(game_on){
+            ball_vel.y = -ball_speed;
+        }else{
+            ball.x = player.x + player.width / 2;
+            ball.y = player.y - player.height / 2;
+        }
+    }
+    ball.x += ball_vel.x;
+    ball.y += ball_vel.y;
+    if(ball.x < 0){
+        ball_vel.x = ball_speed;
+    }else if(ball.x > config.width){
+        ball_vel.x = -ball_speed;
+    }
+    if(ball.y < 0){
+        ball_vel.y = ball_speed;
+    }else if(ball.y > config.height){
+        ball_vel.y = -ball_speed;
+    }
+    if(keys.SPACE.isDown){
+        ball_vel.y = ball_speed;
+        ball_vel.x = ball_speed;
+        game_on = true;
+    }
+}
+
 function update(time, delta){
     let speed = paddle_vel * (delta / 1000);
-    moveByArrow(rect_container, speed);
+    moveByArrow(player, speed);
     if(this.input.mousePointer.leftButtonDown()){
-        rect_container.x = this.input.mousePointer.x - 100;
+        player.x = this.input.mousePointer.x - 100;
     }
-    check_boundaries(rect_container);
+    check_boundaries(player);
+    moveBall(ball_speed * (delta / 1000));
 }
