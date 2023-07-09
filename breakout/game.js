a33c78de371d7c143b73ea232740d6f5c6c855f4
ruby <jhonnatanwfc@live.com>
2023-07-09 08:31:26 -0300

    grid

--- breakout/game.js
+++ breakout/game.js
@@ -15,20 +15,40 @@ let paddle;
 let keys;
 const paddle_vel = 300;
 let mouse_guide = false;
-let rect;
+let player;
+let player_rect;
 let rect_w = 200;
 let rect_h = 30;
 let rect_container;
+//let grid;
+const box_width = 80;
+const box_height = 40;
+const gap = 22;
+let grid = [];
 
 function create(){
     let x = config.width / 2 - rect_w / 2;
     let y = config.height - rect_h - 5;
     keys = this.input.keyboard.addKeys("A,D,RIGHT,LEFT"); 
     rect_container = this.add.container(x, y);
-    rect = this.add.graphics();
-    rect.fillStyle(0x6ed3a2);
-    rect.fillRoundedRect(0, 0, rect_w, rect_h, 10);
-    rect_container.add(rect);
+    player = this.add.graphics();
+    player_rect = new Phaser.Geom.Rectangle(x, y, rect_w, rect_h);
+    player.fillStyle(0x6ed3a2);
+    player.fillRoundedRect(0, 0, rect_w, rect_h, 8);
+    rect_container.add(player);
+    //grid = this.add.container(0, 0);
+    for(let row = 0; row < 5; ++row){
+        grid[row] = [];
+        for(let col = 0; col < config.width / box_width; ++col){
+            let x = col * box_width + col * gap;
+            let y = row * box_height + row * gap;
+            //grid[row][col] = this.add.rectangle(x + box_width / 2, y + box_height / 2, box_width, box_height, 0x89CFF0);
+            let box = this.add.graphics();
+            box.fillStyle(0x89CFF0);
+            box.fillRoundedRect(x, y, box_width, box_height, 15);
+            grid[row][col] = new Phaser.Geom.Rectangle(x, y, box_width, box_height);
+        }
+    }
 }
   
 function check_boundaries(obj){
