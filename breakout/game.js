26cc27cf397fd00dd7ddd0a664fefe83b9f10d6c
ruby <jhonnatanwfc@live.com>
2023-07-18 22:03:26 -0300

    physics implementation 1

--- breakout/game.js
+++ breakout/game.js
@@ -3,157 +3,134 @@ let config = {
     width: 1200,
     height: 700,
     backgroundColor: "#123524",
-    fps: 60,
+    fps: {
+        target: 60,
+        min: 30
+    },
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update,
+        render: render
     },
 };
 
 let game = new Phaser.Game(config);
 let paddle;
-let pallet;
-let pallet_scale = 0.1;
-let pallet_w, pallet_h;
+const paddle_scale = 0.1;
+let paddle_w, paddle_h;
+const paddle_speed = 300;
 let keys;
-const paddle_vel = 500;
-let player;
 let player_w = 200;
 let player_h = 30;
-const box_width = 80;
-const box_height = 40;
-let box_scale_w = 0.85, box_scale_h = 0.5;
+const brick_scale_w = 0.85, brick_scale_h = 0.5;
+let brick_w, brick_h;
 const gap = 22;
 let grid = [];
 let grid_rows = 4;
-let grid_cols = config.width / box_width;
+let grid_cols = 12;//config.width / bricks_width;
 let ball;
 let circle;
-let circle_scale = 0.25;
+const circle_scale = 0.25;
 let circle_radius;
 let ball_speed = 300;
 let ball_vel = {x: 0, y: 0};
 let game_on = false;
 
+function render(){
+    this.game.context.clearRect(0, 0, this.game.canvas.width, this.game.canvas.height);
+}
+
 function preload(){
-    this.load.image("pallet", "assets/pallet.png");
-    this.load.image("circle", "assets/circle.png");
-    this.load.image("box", "assets/box.png");
+    this.load.image("paddle", "assets/paddle.png");
+    this.load.image("ball", "assets/circle.png");
+    this.load.image("brick", "assets/brick.png");
 }
 
 function create(){
-    let player_x = config.width / 2 - player_w / 2;
-    let player_y = config.height - player_h - 5;
-    pallet = this.add.sprite(0, 0, "pallet");
-    circle = this.add.sprite(0, 0, "circle");
-    pallet.setOrigin(0, 0);
-    //circle.setOrigin(0, 0);
-    pallet.setScale(pallet_scale);
-    circle.setScale(circle_scale);
-    pallet_w = pallet.width * pallet_scale;
-    pallet_h = pallet.height * pallet_scale;
-    circle_radius = circle.radius * circle_scale;
-    pallet.x = config.width / 2 - pallet_w / 2;
-    pallet.y = config.height - pallet_h;
-    circle.x = player_x + player_w / 2; 
-    circle.y = player_y - player_h;    
-    //console.log(pallet.x, pallet.y, config.width, config.height);
+    paddle = this.physics.add.sprite(0, 0, "paddle");
+    paddle.setCollideWorldBounds(true);
+    ball = this.physics.add.sprite(0, 0, "ball");
+    ball.setCollideWorldBounds(true);
+    ball.setBounce(1);
+    paddle.setOrigin(0, 0);
+    paddle.setScale(paddle_scale);
+    ball.setScale(circle_scale);
+    paddle_w = paddle.width * paddle_scale;
+    paddle_h = paddle.height * paddle_scale;
+    circle_radius = ball.radius * circle_scale;
+    paddle.x = config.width / 2 - paddle_w / 2;
+    paddle.y = config.height - paddle_h;
+    ball.x = paddle.x + paddle_w / 2; 
+    ball.y = paddle.y - paddle_h / 2;    
     keys = this.input.keyboard.addKeys("A,D,RIGHT,LEFT,SPACE"); 
-    // new Phaser.Geom.Rectangle(x, y, player_w, player_h);
-    //player = this.add.rectangle(player_x, player_y, player_w, player_h, 0x6ed3a2);
-    //player.setOrigin(0, 0);
-    player = pallet;
-    //ball = new Phaser.Geom.Circle(x + player_w / 2, y - player_h / 2, 20);
-    //ball = this.add.circle(player_x + player_w / 2, player_y - player_h / 2, 20, 0xFF8C00);
-    ball = circle;
     for(let row = 0; row < grid_rows; ++row){
         grid[row] = [];
         for(let col = 0; col < grid_cols; ++col){
-            let x = col * box_width + col * gap;
-            let y = row * box_height + row * gap;
-            // new Phaser.Geom.Rectangle(x, y, box_width, box_height);
-            //let box = this.add.rectangle(x, y, box_width, box_height, 0x89CFF0);
-            let box = this.add.sprite(x, y, "box");
-            box.setScale(box_scale_w, box_scale_h);
-            grid[row][col] = box;
-            grid[row][col].setOrigin(0, 0);
+            let brick = this.physics.add.sprite(0, 0, "brick");
+            brick.setOrigin(0, 0);
+            brick.setScale(brick_scale_w, brick_scale_h);
+            brick_w = brick.width * brick_scale_w;
+            brick_h = brick.height * brick_scale_h;
+            brick.x = col * brick_w + col * gap;
+            brick.y = row * brick_h + row * gap;
+            grid[row][col] = brick;
         }
     }
+    this.physics.add.collider(paddle, ball, paddleBallCollision, null, this);
 
-}
-
-
-function check_boundaries(obj, player_w){
-    if(obj.x + player_w > config.width){
-        obj.x = config.width - player_w;
-    }
-    if(obj.x < 0){
-        obj.x = 0;
-    }
-}
-
-function moveByArrow(obj, speed){
-    if(keys.LEFT.isDown || keys.A.isDown){
-        obj.x -= speed;
-    }else if(keys.RIGHT.isDown || keys.D.isDown){
-        obj.x += speed;
-    }
 }
 
 function gridCollision(){
-    for (let row = 0; row < grid_rows; row++){
-        for(let col = 0; col < grid_cols; col++){
+    for (let row = 0; row < grid.length; row++){
+        for(let col = 0; col < grid[row].length; col++){
+            //console.log(row, col);
             let sprite = grid[row][col];
-            if(sprite.active && Phaser.Geom.Intersects.RectangleToRectangle(sprite.getBounds(), ball.getBounds())){
+            if(sprite.visible && Phaser.Geom.Intersects.RectangleToRectangle(sprite.getBounds(), ball.getBounds())){
                 ball_vel.y = -ball_vel.y;
                 ball.y += ball_vel.y;
                 if(ball.y < sprite.y + sprite.height){
                     ball_vel.x = -ball_vel.x;
                 }
                 sprite.setVisible(false);
-                sprite.setActive(false);
+                //sprite.setActive(false);
+                //sprite.destroy();
             }
         }
     }
-
 }
 
-function moveBall(ball_speed, player_w, player_h){
-    if(Phaser.Geom.Intersects.RectangleToRectangle(player.getBounds(), ball.getBounds())){
-        if(game_on){
-            ball_vel.y = -ball_speed;
-        }else{
-            ball.x = player.x + player_w / 2;
-            ball.y = player.y - player_h / 2;
-        }
-    }
-    ball.x += ball_vel.x;
-    ball.y += ball_vel.y;
-    if(ball.x < 0){
-        ball_vel.x = ball_speed;
-    }else if(ball.x > config.width){
-        ball_vel.x = -ball_speed;
-    }
-    if(ball.y < 0){
-        ball_vel.y = ball_speed;
-    }else if(ball.y > config.height){
-        ball_vel.y = -ball_speed;
-    }
-    if(keys.SPACE.isDown){
-        ball_vel.y = ball_speed;
-        ball_vel.x = ball_speed;
-        game_on = true;
-    }
+
+function paddleBallCollision(){
+    ball.setVelocityY(-ball.body.velocity.y);
 }
 
 function update(time, delta){
-    let speed = paddle_vel * (delta / 1000);
-    gridCollision();
-    moveByArrow(player, speed);
-    if(this.input.mousePointer.leftButtonDown()){
-        player.x = this.input.mousePointer.x - 100;
+    if(keys.LEFT.isDown || keys.A.isDown){
+        paddle.setVelocityX(-paddle_speed);
     }
-    check_boundaries(player, pallet_w);
-    moveBall(ball_speed * (delta / 1000), pallet_w, pallet_h);
+    if(keys.RIGHT.isDown || keys.D.isDown){
+        paddle.setVelocityX(paddle_speed);
+    }
+    if(keys.RIGHT.isUp && keys.D.isUp && keys.LEFT.isUp && keys.A.isUp){
+        paddle.setVelocityX(0);
+    }
+    if(paddle.body.touching.left || paddle.body.touching.right){
+        paddle.setVelocityX(0);
+    }
+    if(ball.body.touching.left || ball.body.touching.right){
+        ball.setVelocityX(-ball.body.velocity.x);
+    }
+    if(ball.body.touching.up || ball.body.touching.down){
+        ball.setVelocityY(-ball.body.velocity.y);
+    }
+    if(keys.SPACE.isDown){
+        ball.setVelocity(ball_speed, ball_speed);
+    }
 }
