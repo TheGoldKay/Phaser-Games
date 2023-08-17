c965da5d7cb7a66081740ae7c303c751a247f8df
ruby <jhonnatanwfc@live.com>
2023-08-17 19:12:51 -0300

    dino idle animation

--- dino/dino.js
+++ dino/dino.js
@@ -19,12 +19,25 @@ let game = new Phaser.Game(config);
 
 function preload(){
     this.load.image("dino_idle_1", "assets/sprites/_raw/player/idle/dino-idle-1.png");
+    this.load.image("dino_idle_2", "assets/sprites/_raw/player/idle/dino-idle-2.png");
 }
 
 function create(){
     // Set up the physics system (Arcade Physics)
     this.physics.world.setBounds(0, 0, config.width, config.height);
-    
-    // Create a sprite with physics enabled at (x, y) position using the "dino_idle" image
-    this.physics.add.sprite(400, 300, "dino_idle_1");
+    this.anims.create({
+        key: 'idleAnimation',
+        frames: [
+            { key: 'dino_idle_1' },
+            { key: 'dino_idle_2' },
+        ],
+        frameRate: 1,
+        repeat: -1,
+    });
+
+    this.idle = this.physics.add.sprite(0, 0, 'dino_idle_1');
+    this.idle.setOrigin(0, 0);
+    this.idle.x = config.width / 2 - this.idle.width / 2;
+    this.idle.y = config.height - this.idle.height;
+    this.idle.anims.play('idleAnimation');  // Start playing animation
 }
